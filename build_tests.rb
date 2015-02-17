1d2d067a01b9ff78f6d9f9f09f1f3dc6657ff661
Robert D. French <frenchrd@ornl.gov>
2015-02-16 23:28:16 -0500

    Generate separate shard libraries and executables for each test

--- build_tests.rb
+++ build_tests.rb
@@ -1,10 +1,46 @@
 #!/usr/bin/env ruby
+require "json"
+require "erb"
 
 def run_dmz(cmd)
 	puts cmd
 	`#{cmd}`
 end
 
-(1..10).each do |n|
+def log(msg)
+	puts "[TOAST BUILD] #{msg}"
+end
+
+log "load environment.json"
+env = JSON.load(File.open("environment.json"))
+@account = env['account']
+@nodes = env['nodes']
+@seconds = env['seconds']
+@name = env['name']
+@shared_filesystem_path = env['shared_filesystem_path']
+@node_filesystem_path = env['node_filesystem_path']
+@num_trials = env['num_trials']
+@density = env['density']
+num_executables = @num_trials * 2
+
+log "build executables and shared libs"
+(1..num_executables).each do |n|
 	run_dmz "make build/tests/test_shared-lib#{n}.exe"
 end
+
+log "generate pbs script"
+b = binding
+erb = ERB.new(File.read("tests/script_template.erb"))
+File.open(ARGV[0],"w") do |f|
+	f.write erb.result b
+end
+
+log "Deploy everything to shared filesystem"
+(1..num_executables).each do |n|
+	`cp build/tests/test_shared-lib#{n}.exe #{@shared_filesystem_path}`
+	`cp build/tests/libdummy#{n}.so #{@shared_filesystem_path}`
+end
+`cp toast #{@shared_filesystem_path}`
+
+jobid = `qsub #{ARGV[0]}`
+log "Submitting to queue system: #{jobid}"
--- tests/shared-lib-test/test_shared-lib.cpp
+++ tests/shared-lib-test/test_shared-lib.cpp
@@ -7,14 +7,7 @@ int main(int argc, char** argv) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  double start = MPI_Wtime();
-  std::string mystring = bigString();
-  double invocation_time = MPI_Wtime() - start;
-  int length = mystring.length();
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  std::cout << "{\"rank\": " << rank << ", \"invocation_time\": " << invocation_time << ", \"str_length\": " << length << "}" << std::endl;
+  std::cout << bigString();
   MPI_Finalize();
   return 0;
 }
