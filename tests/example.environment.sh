dc664b4293bf70d29a99084ecbefdc1c4d7a04b4
Robert D. French <frenchrd@ornl.gov>
2015-02-16 19:16:07 -0500

    Remove shell commands

--- tests/example.environment.sh
+++
@@ -1,4 +0,0 @@
-# Move this file to 'tests/environment.sh' when you've set all the variables
-
-# The path to where assets should be placed on the shared fs for testing
-export SCRATCH_PATH=/some/path/on/the/shared/fs
